1f8d630c8f733ea0129c042ee8f9c534f54f2694
SiddhuPudi <thrivikram.pudi00@gmail.com>
2026-04-30 21:55:53 +0530

    feat: Initialized React project with Vite, Tailwind CSS, and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-3xl text-white">
+        Initializing Control Room...
+      </h1>
+    </div>
+  );
+}
+
+export default App;
